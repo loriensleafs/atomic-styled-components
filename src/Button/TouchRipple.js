151363f84a7f4fc5892b611673510ab9a698f975
loriensleafs <pkloss@gmail.com>
2018-07-18 09:19:19 -0400

    Makes ripple handler method. Starts creating TouchRipple component.

---
+++ src/Button/TouchRipple.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
+import { styled } from './../styled';
+import Ripple from './Ripple';
+
+const DURATION = 550;
+export const DELAY_RIPPLE = 80;
