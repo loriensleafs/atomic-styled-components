e0c5739dbc025bb99bcd27ce143c6acec62757f1
loriensleafs <pkloss@gmail.com>
2018-08-07 10:12:02 -0400

    Pulls the Button component out into its own directory and creates a ButtonBase specific component directory.

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { classify, themify } from './../styled';
-import ButtonBase from './ButtonBase';
+import ButtonBase from './../ButtonBase';
 import { fade } from './../utils/colorHelpers';
 
 /**
