1249a9694e84e73699dd2151fe453dfa8beee7ba
loriensleafs <pkloss@gmail.com>
2018-08-18 14:17:13 -0400

    Moves default imports to be first.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import App from './App';
 import { Provider as StyletronProvider } from 'styletron-react';
 import { ThemeProvider, theme } from './theme';
 import { engine } from './themify';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { injectGlobal } from 'styled-components';
 
 injectGlobal`* { box-sizing: border-box; } html { min-height: 100%; } html,body,#root {  position: relative; height: 100%; margin: 0; padding: 0; }`;
