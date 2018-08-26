07009a243621074834f6bcf4383fd8f159e3806a
loriensleafs <pkloss@gmail.com>
2018-08-26 15:00:22 -0400

    Updates App to demo Card and Paper components.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ErrorBoundry from './ErrorBoundry';
 import Flex from './Flex';
 import Text from './Text';
 import Button from './Button';
+import Card from './Card';
 import IconButton from './IconButton';
 import SvgIcon from './SvgIcon';
 
@@ -14,59 +15,61 @@ const App = (props) => {
 	return (
 		<ErrorBoundry>
 			<Box width={1} height={1} bg="bg.default">
-				<Box p={4} maxWidth={1024}>
-					<Flex flexWrap="wrap" mt={4} mx={2}>
-						<Box width={1} pt={2} pb={4} px={2}>
-							<Button variant="contained" color="primary">
-								Button
-							</Button>
-						</Box>
-						<Box px={2} py={1} width={1 / 2}>
-							<Text p={1} color="text.contrast.primary" bg="primary.main">
-								1/2
-							</Text>
-						</Box>
-						<Box px={2} py={1} width={1 / 2}>
-							<Text p={1} color="text.contrast.primary" bg="primary.main">
-								1/2
-							</Text>
-						</Box>
-						<Box px={2} py={1} width={1 / 3}>
-							<Text p={1} color="text.contrast.primary" bg="primary.main">
-								1/3
-							</Text>
-						</Box>
-						<Box px={2} py={1} width={1 / 3}>
-							<Text p={1} color="text.contrast.primary" bg="primary.main">
-								1/3
-							</Text>
-						</Box>
-						<Box px={2} py={1} width={1 / 3}>
-							<Text p={1} color="text.contrast.primary" bg="primary.main">
-								1/3
-							</Text>
-						</Box>
-						<Box px={2} py={1} width={1 / 4}>
-							<Text p={1} color="text.contrast.primary" bg="primary.main">
-								1/4
-							</Text>
-						</Box>
-						<Box px={2} py={1} width={1 / 4}>
-							<Text p={1} color="text.contrast.primary" bg="primary.main">
-								1/4
-							</Text>
-						</Box>
-						<Box px={2} py={1} width={1 / 4}>
-							<Text p={1} color="text.contrast.primary" bg="primary.main">
-								1/4
-							</Text>
-						</Box>
-						<Box px={2} py={1} width={1 / 4}>
-							<Text p={1} color="text.contrast.primary" bg="primary.main">
-								1/4
-							</Text>
-						</Box>
-					</Flex>
+				<Box maxWidth={1024} mx="auto" py={3}>
+					<Card>
+						<Flex flexWrap="wrap" mx={2} p={4}>
+							<Box width={1} pt={2} pb={4} px={2}>
+								<Button variant="contained" color="primary">
+									Button
+								</Button>
+							</Box>
+							<Box px={2} py={1} width={1 / 2}>
+								<Text p={1} color="text.contrast.primary" bg="primary.main">
+									1/2
+								</Text>
+							</Box>
+							<Box px={2} py={1} width={1 / 2}>
+								<Text p={1} color="text.contrast.primary" bg="primary.main">
+									1/2
+								</Text>
+							</Box>
+							<Box px={2} py={1} width={1 / 3}>
+								<Text p={1} color="text.contrast.primary" bg="primary.main">
+									1/3
+								</Text>
+							</Box>
+							<Box px={2} py={1} width={1 / 3}>
+								<Text p={1} color="text.contrast.primary" bg="primary.main">
+									1/3
+								</Text>
+							</Box>
+							<Box px={2} py={1} width={1 / 3}>
+								<Text p={1} color="text.contrast.primary" bg="primary.main">
+									1/3
+								</Text>
+							</Box>
+							<Box px={2} py={1} width={1 / 4}>
+								<Text p={1} color="text.contrast.primary" bg="primary.main">
+									1/4
+								</Text>
+							</Box>
+							<Box px={2} py={1} width={1 / 4}>
+								<Text p={1} color="text.contrast.primary" bg="primary.main">
+									1/4
+								</Text>
+							</Box>
+							<Box px={2} py={1} width={1 / 4}>
+								<Text p={1} color="text.contrast.primary" bg="primary.main">
+									1/4
+								</Text>
+							</Box>
+							<Box px={2} py={1} width={1 / 4}>
+								<Text p={1} color="text.contrast.primary" bg="primary.main">
+									1/4
+								</Text>
+							</Box>
+						</Flex>
+					</Card>
 				</Box>
 			</Box>
 		</ErrorBoundry>
