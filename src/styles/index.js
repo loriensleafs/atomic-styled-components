ea4f6b17a324ac5ccca391aee497f3a80ea77671
loriensleafs <pkloss@gmail.com>
2018-12-01 11:48:45 -0500

    Exposes text style parsers.

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,4 +1,5 @@
 export { bgColor, textColor } from './color';
 export { borderRadius } from './border';
 export { height, maxHeight, minHeight } from './height';
+export { fontFamily, fontSize, fontWeight, lineHeight } from './text';
 export { maxWidth, minWidth, width } from './width';
