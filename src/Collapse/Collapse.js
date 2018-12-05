0a7b82451d8a161622da5b43a7ebc9f8026dceb4
loriensleafs <pkloss@gmail.com>
2018-12-05 12:33:16 -0500

    Adds a Collapse component that animates the expanding/collapsing of a component's height.

---
+++ src/Collapse/Collapse.js
@@ -0,0 +1,64 @@
+import React, { useCallback, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { animated, useSpring } from 'react-spring';
+
+function Collapse(props) {
+	const {
+		children,
+		collapsedHeight,
+		component: Component,
+		in: inProp,
+		onEnd,
+		onStart,
+		...passThru
+	} = props;
+	const ref = useRef();
+	const handleStart = useCallback(() => onStart && onStart(), []);
+	const handleEnd = useCallback(() => onEnd && onEnd(), []);
+	const [transition] = useSpring({
+		height: inProp ? ref.current.clientHeight : collapsedHeight,
+		from: {
+			height: collapsedHeight,
+		},
+		onStart: handleStart,
+		onRest: handleEnd,
+	});
+
+	return (
+		<animated.div style={{ ...transition, overflow: 'hidden' }} {...passThru}>
+			<Component ref={ref}>{children}</Component>
+		</animated.div>
+	);
+}
+
+Collapse.displayName = 'Collapse';
+
+Collapse.propTypes = {
+	/**
+	 * The content node to be collapsed.
+	 */
+	children: PropTypes.node,
+	/**
+	 * The height of the container when collapsed.
+	 */
+	collapsedHeight: PropTypes.string,
+	/**
+	 * If `true`, the component will transition in.
+	 */
+	in: PropTypes.bool,
+	/**
+	 * Callback that is triggered at the end of the animation.
+	 */
+	onEnd: PropTypes.func,
+	/**
+	 * Callback that is triggered at the start of the animation.
+	 */
+	onStart: PropTypes.func,
+};
+
+Collapse.defaultProps = {
+	collapsedHeight: 0,
+	component: 'div',
+};
+
+export default Collapse;
