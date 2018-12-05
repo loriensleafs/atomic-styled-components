bc83801f7c274f5216232c71ebb48f9cbf0f70f2
loriensleafs <pkloss@gmail.com>
2018-12-05 12:33:48 -0500

    Adds proptypes and onStart onEnd callbacks to the Fade transition component.

--- src/Fade/Fade.js
+++ src/Fade/Fade.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
 import { animated, useSpring } from 'react-spring';
 
 function Fade(props) {
+	const { children, in: inProp, onEnd, onStart, ...passThru } = props;
+	const handleStart = useCallback(() => onStart && onStart(), []);
+	const handleEnd = useCallback(() => onEnd && onEnd(), []);
 	const [transition] = useSpring({
-		opacity: props.in ? 1 : 0,
+		opacity: inProp ? 1 : 0,
 		from: {
-			opacity: props.in ? 0 : 1,
+			opacity: inProp ? 0 : 1,
 		},
+		onStart: handleStart,
+		onRest: handleEnd,
 	});
 
-	return <animated.div style={transition}>{props.children}</animated.div>;
+	return (
+		<animated.div style={transition} {...passThru}>
+			{children}
+		</animated.div>
+	);
 }
 
 Fade.displayName = 'Fade';
 
-Fade.propTypes = {};
+Fade.propTypes = {
+	/**
+	 * A single child content element.
+	 */
+	children: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
+	/**
+	 * If `true`, the component will transition in.
+	 */
+	in: PropTypes.bool,
+	/**
+	 * Callback that is triggered at the end of the animation.
+	 */
+	onEnd: PropTypes.func,
+	/**
+	 * Callback that is triggered at the start of the animation.
+	 */
+	onStart: PropTypes.func,
+};
 
 export default Fade;
