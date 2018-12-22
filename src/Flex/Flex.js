1655bb743196d31c3f6484e62c7551cba9ee4cf1
loriensleafs <pkloss@gmail.com>
2018-12-22 12:21:21 -0500

    Removes Border component.  Moves any Flex or Grid layout style parsers into the Flex and Grid components and out of the Box component, as the Flex and Grid components compose the Box component.

--- src/Flex/Flex.js
+++ src/Flex/Flex.js
@@ -1,79 +1,132 @@
-import React, { useContext, useMemo } from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
-import ThemeContext from './../theme/ThemeContext';
 import Box from '../Box';
 import useStyles from './../hooks/useStyles';
 import {
-	alignContent,
-	alignItems,
-	alignSelf,
-	flex,
-	flexBasis,
-	justifyContent,
-	justifySelf,
-	order,
+	alignContent as alignContentParser,
+	alignItems as alignItemsParser,
+	alignSelf as alignSelfParser,
+	flex as flexParser,
+	justifyContent as justifyContentParser,
+	justifyItems as justifyItemsParser,
+	justifySelf as justifySelfParser,
+	order as orderParser,
 	style,
 } from 'styled-system';
 
-export const flexDisplay = style({
-	prop: 'inline',
-	cssProperty: 'display',
-	transformValue: n => (!n ? 'flex' : 'inline-flex'),
+export const flexBasis = style({
+	prop: 'basis',
+	cssProperty: 'flexBasis',
 });
 
 export const flexDirection = style({
-	prop: 'direction',
+	prop: 'dir',
 	cssProperty: 'flexDirection',
-	transformValue: n => (n === 'col' ? 'column' : 'row'),
+	transformValue: n => (n === 'col' || n === 'column' ? 'column' : 'row'),
+});
+
+export const flexDisplay = props =>
+	props.child ? null : props.inline ? { display: 'inline-flex' } : { display: 'flex' };
+
+export const flexGrow = style({
+	prop: 'grow',
+	cssProperty: 'flexDirection',
+});
+
+export const flexShrink = style({
+	prop: 'shrink',
+	cssProperty: 'flexShrink',
 });
 
 export const flexWrap = style({
-	prop: 'flexWrap',
+	prop: 'wrap',
+	cssProperty: 'flexWrap',
+	transformValue: n => n && 'wrap',
 });
 
-function Flex(props) {
-	const { theme } = useContext(ThemeContext);
+const Flex = forwardRef((props, ref) => {
+	const {
+		alignContent,
+		alignItems,
+		alignSelf,
+		basis,
+		child,
+		children,
+		dir,
+		flex,
+		grow,
+		justifyContent,
+		justifyItems,
+		justifySelf,
+		order,
+		shrink,
+		styles: stylesProp,
+		wrap,
+		...passThru
+	} = props;
 	const styles = useStyles(
 		[
-			alignContent,
-			alignItems,
-			alignSelf,
-			flex,
+			alignContentParser,
+			alignItemsParser,
+			alignSelfParser,
+			flexParser,
 			flexBasis,
 			flexDisplay,
 			flexDirection,
+			flexGrow,
+			flexShrink,
 			flexWrap,
+			justifyContentParser,
+			justifyItemsParser,
+			justifySelfParser,
+			orderParser,
+		],
+		{
+			alignContent,
+			alignItems,
+			alignSelf,
+			basis,
+			child,
+			dir,
+			flex,
+			grow,
 			justifyContent,
+			justifyItems,
 			justifySelf,
 			order,
-		],
-		{ ...props, theme },
+			shrink,
+			styles: stylesProp,
+			wrap,
+		},
 	);
 
 	return (
-		<Box styles={styles} {...props}>
-			{props.children}
+		<Box ref={ref} styles={styles} {...passThru}>
+			{children}
 		</Box>
 	);
-}
+});
 
 Flex.displayName = 'Flex';
 
 Flex.propTypes = {
-	inline: PropTypes.bool,
-	...alignContent.propTypes,
-	...alignItems.propTypes,
-	...alignSelf.propTypes,
-	...flex.propTypes,
+	...alignContentParser.propTypes,
+	...alignItemsParser.propTypes,
+	...alignSelfParser.propTypes,
+	...flexParser.propTypes,
 	...flexBasis.propTypes,
 	...flexDirection.propTypes,
+	...flexGrow.propTyeps,
+	...flexShrink.propTypes,
 	...flexWrap.propTypes,
-	...justifyContent.propTypes,
-	...justifySelf.propTypes,
-	...order.propTypes,
-	...{
-		styles: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-	},
+	...justifyContentParser.propTypes,
+	...justifyItemsParser.propTypes,
+	...justifySelfParser.propTypes,
+	...orderParser.propTypes,
+	child: PropTypes.bool,
+	children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+	inline: PropTypes.bool,
+	styles: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
 };
 
 Flex.defaultProps = {
