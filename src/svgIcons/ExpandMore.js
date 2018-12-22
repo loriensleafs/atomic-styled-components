3cdf1af28727ef9aa6bc9ef3f10d78f23d4185b5
loriensleafs <pkloss@gmail.com>
2018-12-22 11:09:12 -0500

    Adds displayName to ExpandMore SvgIcon component.

--- src/svgIcons/ExpandMore.js
+++ src/svgIcons/ExpandMore.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import SvgIcon from './../SvgIcon';
 
-export default props => (
+const ExpandMore = props => (
 	<SvgIcon {...props}>
 		<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
 		<path fill="none" d="M0 0h24v24H0z" />
 	</SvgIcon>
 );
+
+ExpandMore.displayName = 'ExpandMoreIcon';
+
+export default ExpandMore;
