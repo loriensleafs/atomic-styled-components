dd3514f44e49d9429d8b92f2e26dbbadf8b01ecd
loriensleafs <pkloss@gmail.com>
2018-12-26 20:59:34 -0500

    Updates comments for useWillUnmount hook.

--- src/hooks/useWillUnmount.js
+++ src/hooks/useWillUnmount.js
@@ -1,16 +1,12 @@
 import { useEffect } from 'react';
+import { isFunc } from './../utils/helpers';
 
 /**
  * Triggers the function when the component is unmounted.
- * @param {Function} fn - Callback to run after the component is unmounted.
- * @param {Function} hook [useEffect] - By default the 'useEffect' hook is used,
+ * @param {function} cb Callback to run after the component is unmounted.
+ * @param {function} hook [useEffect] By default the 'useEffect' hook is used,
  * but it can be replaced by the 'useLayoutEffect' or 'useMutationEffect' hook.
- * @return {void}
  */
-export default function useWillUnmount(fn, hook = useEffect) {
-	hook(() => {
-		if (fn && typeof fn === 'function') {
-			fn();
-		}
-	}, []);
+export default function useWillUnmount(cb, hook = useEffect) {
+	hook(() => cb && isFunc(cb) && cb(), []);
 }
