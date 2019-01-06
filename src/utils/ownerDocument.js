4a45173f663311369842c813d155ebc722b4a1b2
loriensleafs <pkloss@gmail.com>
2019-01-05 19:00:44 -0500

    Consistent method organization.

--- src/utils/ownerDocument.js
+++ src/utils/ownerDocument.js
@@ -1,3 +1,5 @@
-export default function ownerDocument(node) {
+function ownerDocument(node) {
 	return (node && node.ownerDocument) || document;
 }
+
+export default ownerDocument;
--- src/utils/ownerWindow.js
+++ src/utils/ownerWindow.js
@@ -1,7 +1,8 @@
 import ownerDocument from './ownerDocument';
 
-export default function ownerWindow(node, fallback = window) {
+function ownerWindow(node, fallback = window) {
 	const doc = ownerDocument(node);
-
 	return doc.defaultView || doc.parentView || fallback;
 }
+
+export default ownerWindow;
