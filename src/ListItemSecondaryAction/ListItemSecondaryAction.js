3043e882cfbfb7572535cc58b749405dc222acc0
loriensleafs <pkloss@gmail.com>
2019-02-16 22:39:32 -0500

    Updates ListItemSecondaryAction to handle styling with the useStyles hook.

--- src/ListItemSecondaryAction/ListItemSecondaryAction.js
+++ src/ListItemSecondaryAction/ListItemSecondaryAction.js
@@ -1,42 +1,18 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import cn from '../system/className';
-import merge from '../utils/merge';
-import { isFn } from '../utils/helpers';
+import useStyles from '../system/useStyles';
 import { stylesPropType } from '../utils/propTypes';
 
 const baseStyles = {
 	position: 'absolute',
 	top: '50%',
 	right: '4px',
-	transform: 'translate(-50%)',
+	transform: 'translateX(-50%) translateY(-50%)',
 };
 
 function ListItemSecondaryAction(props) {
-	const {
-		children,
-		className: classNameProp,
-		styles: stylesProp,
-		...passThru
-	} = props;
-	const styles = useMemo(
-		() =>
-			merge(
-				baseStyles,
-				isFn(stylesProp) ? stylesProp(props) : stylesProp || {},
-			),
-		[props],
-	);
-	const className = useMemo(() => cn(classNameProp, styles), [
-		classNameProp,
-		styles,
-	]);
-
-	return (
-		<div className={className} {...passThru}>
-			{children}
-		</div>
-	);
+	const [{ classes }, passThru] = useStyles(props, null, { baseStyles });
+	return <div className={classes} {...passThru} />;
 }
 
 ListItemSecondaryAction.displayName = 'ListItemSecondaryAction';
