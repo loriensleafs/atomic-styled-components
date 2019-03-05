2155a40f1cf5afae19bfb60a479d81e074945f09
loriensleafs <pkloss@gmail.com>
2019-03-04 19:48:47 -0500

    Removes useSizes from hooks index.js exports.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,10 +1,10 @@
 export { default as useDidMount } from './useDidMount';
 export { default as useDidUpdate } from './useDidUpdate';
 export { default as useInput } from './useInput';
+export { default as useMeasure } from './useMeasure';
 export { default as useMedia } from './useMedia';
 export { default as useMounted } from './useMounted';
 export { default as useMotion } from './useMotion';
 export { default as usePrevious } from './usePrevious';
 export { default as useScrollbarSize } from './useScrollbarSize';
-export { default as useSize } from './useSize';
 export { default as useWillUnmount } from './useWillUnmount';
