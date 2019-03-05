b46a3ffaeaf4fd25876d34c1649094f8cf6b4b8c
loriensleafs <pkloss@gmail.com>
2019-03-04 19:44:15 -0500

    More terse formatting.

--- src/hooks/usePrevious.js
+++ src/hooks/usePrevious.js
@@ -2,8 +2,6 @@ import { useEffect, useRef } from 'react';
 
 export default function usePrevious(value) {
 	const ref = useRef();
-	useEffect(() => {
-		ref.current = value;
-	});
+	useEffect(() => void (ref.current = value), [value]);
 	return ref.current;
 }
