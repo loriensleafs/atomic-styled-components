138c96a41cc361463db61fae408baf3ec857c9c7
loriensleafs <pkloss@gmail.com>
2019-03-04 19:44:54 -0500

    More verbose version of useMotion.

--- src/hooks/useMotion.js
+++ src/hooks/useMotion.js
@@ -3,17 +3,16 @@ import ThemeContext from '../theme/ThemeContext';
 
 function useMotion(easing = 'inOut', enter = 'standard', exit, state = 'show') {
 	const { durations, getEasing } = useContext(ThemeContext).theme;
+	const ease = useMemo(() => getEasing(easing), [easing]);
+	const duration = useMemo(
+		() =>
+			enter && exit && state
+				? durations[state === 'show' ? enter : exit]
+				: durations[enter],
+		[enter, exit, state],
+	);
 
-	return [
-		useMemo(() => getEasing(easing), [easing]),
-		useMemo(
-			() =>
-				enter && exit && state
-					? durations[state === 'show' ? enter : exit]
-					: durations[enter],
-			[enter, exit, state],
-		),
-	];
+	return [ease, duration];
 }
 
 export default useMotion;
