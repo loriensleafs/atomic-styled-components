35beb6559c94772c6af2d53a02ea96b3dbebc3a5
loriensleafs <pkloss@gmail.com>
2019-03-25 01:30:09 -0400

    Removed isAdornedStart utility method as it's for server side rendering and we're not supporting that.

--- src/InputBase/utils.js
+++ src/InputBase/utils.js
@@ -2,5 +2,3 @@ export const hasValue = value =>
 	value != null && !(Array.isArray(value) && value.length === 0);
 
 export const isFilled = obj => obj && (hasValue(obj.value) && obj.value !== '');
-
-export const isAdornedStart = obj => obj.startAdornment;
