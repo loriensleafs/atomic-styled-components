6bfabd3b041c24893ba77c17fd259dc47d2ba71b
loriensleafs <pkloss@gmail.com>
2019-03-25 01:24:37 -0400

    Fixes state management with FormControl integration and adjusts styles.

--- src/FormLabel/FormLabel.js
+++ src/FormLabel/FormLabel.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
-import useFormControlManager from '../FormControl/useFormControlManager';
+import useFormControl from '../FormControl/useFormControl';
 import useStyles from '../system/useStyles';
 import combine from '../utils/combine';
 import { componentPropType, stylesPropType } from '../utils/propTypes';
 
+const getBaseStyles = props => ({
+	root: {
+		padding: '0px',
+		color: props.theme.palette.text.secondary,
+		fontFamily: props.theme.typography.fontFamilies.ui,
+		fontSize: props.theme.typography.fontSizes[3],
+		lineHeight: 1,
+	},
+	asterisk: {},
+});
+
 const getDisabledStyles = ({ disabled, theme: { palette } }) =>
 	disabled && {
-		color: palette.text.disabled,
+		root: {
+			color: palette.text.disabled,
+		},
 	};
 
-const getErrorStyles = ({ error, theme: { palette } }) =>
-	error &
-	{
+const getErrorStyles = ({ disabled, error, theme: { palette } }) =>
+	!disabled &&
+	error && {
 		root: {
 			color: palette.error.main,
 		},
@@ -21,65 +34,57 @@ const getErrorStyles = ({ error, theme: { palette } }) =>
 		},
 	};
 
-const getFocusedStyles = ({ focused, theme: { palette } }) =>
+const getFocusedStyles = ({ disabled, focused, theme: { palette } }) =>
+	!disabled &&
 	focused && {
-		color: palette.primary[palette.type === 'light' ? 'dark' : 'light'],
+		root: {
+			color: palette.primary[palette.type === 'light' ? 'dark' : 'light'],
+		},
 	};
 
-const getBaseStyles = props => ({
-	root: {
-		padding: '0px',
-		color: props.theme.palette.text.secondary,
-		fontFamily: props.theme.typography.fontFamlies.ui,
-		fontSize: props.theme.typography.fontSizes[3],
-		lineHeight: 1,
-		...getFocusedStyles(props),
-		...getDisabledStyles(props),
-	},
-	asterisk: {},
-});
-
-const getStyles = combine(getBaseStyles, getErrorStyles);
+const getStyles = combine(
+	getBaseStyles,
+	getDisabledStyles,
+	getFocusedStyles,
+	getErrorStyles,
+);
 getStyles.propTypes = {
 	// If `true`, the label should be displayed in a disabled state.
 	disabled: PropTypes.bool,
 	// If `true`, the label should be displayed in an error state.
 	error: PropTypes.bool,
+	// If `true`, the label should use filled classes key.
+	filled: PropTypes.bool,
 	// If `true`, the label input is focused (used by `FormGroup` components).
 	focused: PropTypes.bool,
+	// If `true`, the label will indicate that the input is required.\
+	required: PropTypes.bool,
 };
 
-function FormLabel(props) {
-	const [mergedProps] = useFormControlManager(props, [
+const FormLabel = forwardRef((props, ref) => {
+	const { formControlEnabled, ...fc } = useFormControl(props, [
 		'error',
 		'disabled',
 		'filled',
 		'focused',
 		'required',
 	]);
-	const [
-		{ classes },
-		{
-			as: Component,
-			children,
-			disabled,
-			error,
-			filled,
-			focued,
-			required,
-			...passThru
-		},
-	] = useStyles(mergedProps, getStyles, {
-		whitelist: ['disabled', 'error', 'focused'],
+	const {
+		classes,
+		props: { as: Component, children, filled, required, ...passThru },
+	} = useStyles({ ...props, ...fc }, getStyles, {
+		nested: true,
 	});
 
 	return (
-		<Component className={classes.root} {...passThru}>
+		<Component className={classes.root} ref={ref} {...passThru}>
 			{children}
-			{required && <span className={classes.asterisk}>{'\u2009*'}</span>}
+			{fc.required && (
+				<span className={classes.asterisk}>{'\u2009*'}</span>
+			)}
 		</Component>
 	);
-}
+});
 
 FormLabel.displayName = 'FormLabel';
 
@@ -90,12 +95,8 @@ FormLabel.propTypes = {
 	 * Override or extend the styles applied to the component.
 	 * See [CSS API](#css-api) below for more details.
 	 */
-	classes: PropTypes.object.isRequired,
+	classes: PropTypes.object,
 	className: PropTypes.string,
-	// If `true`, the label should use filled classes key.
-	filled: PropTypes.bool,
-	// If `true`, the label will indicate that the input is required.\
-	required: PropTypes.bool,
 	...componentPropType,
 	...stylesPropType,
 	...getStyles.propTypes,
